97f225aedf4d073da08ebbcff44b849123fc37a2
maomincoding <17864296568@163.com>
2020-05-13 12:00:24 +0800

    2020.5.13 12:00

--- app/src/hook/useMemo/index.jsx
+++ app/src/hook/useMemo/index.jsx
@@ -1,21 +1,30 @@
-import React,{useMemo,useEffect} from 'react'
+import React,{useMemo,useState} from 'react'
 
 
 const UseMemo = () => {
-    useMemo(()=>{
-        console.log("useMemo");
-    },[])
-    useEffect(()=>{
-        console.log("useEffect");
-    })
+    const [count,setCount] = useState(0);
+    const [num,setNum] = useState(0);
+    let res = useMemo(()=>{
+        return {count,num} 
+    },[count])
+    // useEffect(()=>{
+    //     console.log("useEffect");
+    // })
     return (
-        <div></div>
+        <div>
+           <div>count：{res.count}</div>
+           <button onClick={()=>{setCount(count+1);console.log(count);}}>change-count</button>
+           <div>num：{res.num}</div>
+           <button onClick={()=>{setNum(num+1);console.log(num);}}>change-num</button>
+        </div>
     )
 }
 
 export default UseMemo
 
 /*
-useEffect与useMemo执行时间不同
+使用 useMemo 避免重复渲染页面。
 
+useEffect与useMemo执行时间不同
+useMemo 是在组件渲染过程中执行的，useEffect是在componentDidMount以后执行的。也就是说useMemo先于useEffect执行。
 */ 
